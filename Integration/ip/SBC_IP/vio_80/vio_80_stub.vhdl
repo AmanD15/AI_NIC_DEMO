203af37a39e811770d70fd71420796a6033af1cf
siddhant23tomar <siddhant23tomar@gmail.com>
2023-10-18 08:47:07 +0530

    New IP added

---
+++ Integration/ip/SBC_IP/vio_80/vio_80_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.1 (lin64) Build 1846317 Fri Apr 14 18:54:47 MDT 2017
+-- Date        : Tue Oct 17 14:27:06 2023
+-- Host        : ajit-System-Product-Name running 64-bit Ubuntu 20.04.6 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/harshad/project_2/project_2.srcs/sources_1/ip/vio_80/vio_80_stub.vhdl
+-- Design      : vio_80
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7k325tffg900-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity vio_80 is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe_in0 : in STD_LOGIC_VECTOR ( 1 downto 0 );
+    probe_out0 : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end vio_80;
+
+architecture stub of vio_80 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe_in0[1:0],probe_out0[0:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "vio,Vivado 2017.1";
+begin
+end;
