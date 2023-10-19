4a128214401f6abfe8de2ce7178d50c42d87f021
siddhant23tomar <siddhant23tomar@gmail.com>
2023-10-19 16:49:19 +0530

    comment updated : nic_mac_resetn is active high

--- Networking/NIC_GLUE_LOGIC/nic_mac_pipe_reset/nic_mac_pipe_reset.v
+++ Networking/NIC_GLUE_LOGIC/nic_mac_pipe_reset/nic_mac_pipe_reset.v
@@ -21,7 +21,7 @@ begin
 		reset_reg <= ENABLE_MAC_pipe_data;	
 	end
 
-	nic_to_mac_resetn <= ~reset_reg; //  nic_to_mac_resetn is active low!
+	nic_to_mac_resetn <= ~reset_reg; //  nic_to_mac_resetn is active high! 
 end
 
 endmodule
