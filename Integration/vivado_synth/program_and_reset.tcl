16a5dadbd711b5698ca7298caf7d0b9429ad98cf
Siddhant_120 <siddhant23tomar@gmail.com>
2023-12-04 16:20:22 +0530

    older tcl files to reset VIO added

---
+++ Integration/vivado_synth/program_and_reset.tcl
@@ -0,0 +1,28 @@
+## open hardware manager
+open_hw
+## connect to serever
+connect_hw_server
+## Open a connection to a hardware target on the hardware server
+open_hw_target
+
+## set vio probes file
+set_property PROBES.FILE {./system.ltx} [get_hw_devices xcvu37p_0]              
+set_property FULL_PROBES.FILE {./system.ltx} [get_hw_devices xcvu37p_0]         
+
+## set Program file
+set_property PROGRAM.FILE {./system.bit} [get_hw_devices xcvu37p_0]    
+#Program Device
+program_hw_devices [get_hw_devices xcvu37p_0]
+
+
+# refresh device
+refresh_hw_device [lindex [get_hw_devices xcvu37p_0] 0]
+
+
+# reset_proc = '1'
+set_property OUTPUT_VALUE 1 [get_hw_probes reset_proc -of_objects [get_hw_vios -of_objects [get_hw_devices xcvu37p_0] -filter {CELL_NAME=~"virtual_reset"}]]
+commit_hw_vio [get_hw_probes {reset_proc} -of_objects [get_hw_vios -of_objects [get_hw_devices xcvu37p_0] -filter {CELL_NAME=~"virtual_reset"}]]            
+
+# reset_proc = '0'
+set_property OUTPUT_VALUE 0 [get_hw_probes reset_proc -of_objects [get_hw_vios -of_objects [get_hw_devices xcvu37p_0] -filter {CELL_NAME=~"virtual_reset"}]]
+commit_hw_vio [get_hw_probes {reset_proc} -of_objects [get_hw_vios -of_objects [get_hw_devices xcvu37p_0] -filter {CELL_NAME=~"virtual_reset"}]]            
