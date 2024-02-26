6905b6ba7a408cc8e253ef2a1137defc17098e32
Indrahas <ihreddy22@gmail.com>
2024-02-26 20:37:37 +0530

    lwip updated

---
+++ lwip/ajitLwip/include/sys_arch.h
@@ -0,0 +1,17 @@
+#ifndef LWIP_SYS_ARCH_H
+#define LWIP_SYS_ARCH_H
+#include "arch/cc.h"
+
+#define SYS_MBOX_NULL   NULL
+#define SYS_SEM_NULL    NULL
+
+typedef void * sys_prot_t;
+
+typedef void * sys_sem_t;
+
+typedef void * sys_mbox_t;
+
+typedef void * sys_thread_t;
+
+u32_t sys_now(void);
+#endif
---
+++ lwip/ajitLwip/src/sys_arch.c
@@ -0,0 +1,6 @@
+#include "arch/cc.h"
+u32_t
+sys_now(void)
+{
+  return 0;
+}
