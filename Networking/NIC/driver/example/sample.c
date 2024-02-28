2101cbde180fdf0526da580d4d62d4d0b992d221
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2024-02-28 18:06:58 +0530

    Finished first cut thorough test of rationalied NIC implementation.   Several flaws were found, chiefly in consistent locking across the HW and SW.

---
+++ Networking/NIC/driver/example/sample.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include "cortos.h"
+#include "nic_driver.h"
+
+int main(int argc, char* argv[])
+{
+
+	// For each NIC in the system {
+	//     allocate a Nic Cortos Queue for the
+	//     free-queue. from the NCRAM 
+	//
+	//      also remember to allocate the lock
+	//      and bget-buffer for the free queue
+	//
+	//	also remember to initialize each
+	//      allocated queue
+	// }
+	
+	
+	
+	// For each NIC {
+	//   For each server {
+	//       allocate Tx, Rx,  Nic Cortos Queues 
+	//
+	// 	 also remember to allocate the lock
+	// 	 and bget-buffer for each rx, tx queue.
+	//
+	//	 also remember to initialize each
+	//       allocated queue
+	//   }
+ 	// }
+
+	// For each NIC {
+	//   set up the NIC config data structure
+	//   for each NIC in the system.
+	// }
+
+
+	// For each NIC {
+	//     configure it
+	// }
+
+
+	// Now enale all the NICs and enjoy.
+}
--- Networking/NIC/driver/include/nic_driver.h
+++ Networking/NIC/driver/include/nic_driver.h
@@ -1,6 +1,10 @@
 #ifndef NIC_DRIVER_H___
 #define NIC_DRIVER_H___
 
+#ifdef USE_CORTOS
+#include "cortos.h"
+#endif
+
 //  Register definitions..  These must be consistent
 //  with the parameters.aa file in the NIC AA source
 #define    P_NIC_CONTROL_REGISTER_INDEX      0
@@ -16,6 +20,7 @@
 #define    P_RX_PKT_COUNT_REGISTER_INDEX     211
 #define    P_STATUS_REGISTER_INDEX           212
 
+// flags.
 #define    F_ENABLE_NIC			      0x1
 #define    F_ENABLE_NIC_INTERRUPT	      0x2
 
@@ -23,11 +28,13 @@
 #define    TXQUEUE				1
 #define    RXQUEUE				2
 
-// This should be aligned to a quad-word.
+#define    NIC_MAX_NUMBER_OF_SERVERS		8
+
+// This should be allocated only from non-cacheable NCRAM.
 typedef struct __NicPacketBuffer {
 
 	// unused   buffer-size   pkt-length   last-dword-byte-mask
-	// 	     in dwords	   in dwords
+	// 	     in dwords	   in bytes
 	// [63:32]   [31:20]        [19:8]           [7:0]
 	// 
 	uint64_t control_word;
@@ -35,9 +42,7 @@ typedef struct __NicPacketBuffer {
 	uint64_t ethernet_header_h;
 	uint64_t ethernet_header_l;
 
-	// max packet length = 2048 bits
-	// room to spare.
-	uint64_t packet_data [32];
+	uint64_t* packet_data;
 
 	// time-stamps.
 	uint64_t nic_arrival_time_stamp;
@@ -49,6 +54,7 @@ typedef struct __NicPacketBuffer {
 } NicPacketBuffer;
 
 
+#ifndef USE_CORTOS
 // This is a mirror of the cortos-queue data
 // structure.  Note that the lock and buffer
 // addresses are virtual.   The physical addresses
@@ -59,8 +65,8 @@ typedef struct __NicCortosQueue {
   uint32_t writeIndex;				 // + 8 
   uint32_t length;				 // + 12 
   uint32_t msgSizeInBytes;			 // + 16   
-  uint32_t lock_virtual_address;	         // + 20		    
-  uint32_t buffer_virtual_address;               // + 24
+  uint8_t *lock;	         		 // + 20		    
+  uint8_t *bget_addr;               			 // + 24
    // if misc == 1, then assume single writer and single reader and don't use locks
    //    Use misc[31:24] to keep server id
    //    Use misc[23:16] to keep queue-type 
@@ -69,6 +75,48 @@ typedef struct __NicCortosQueue {
   uint32_t misc;		                 // + 28
 	
 } NicCortosQueue;
+#else
+typedef CortosQueueHeader NicCortosQueue;
+#endif
+
+void initNicCortosQueue (NicCortosQueue* cqueue,
+				uint32_t queue_capacity,
+				uint32_t message_size_in_bytes,
+				uint8_t* lock,
+				uint8_t* bget_addr,
+				uint32_t misc);
+
+
+typedef struct __NicConfiguration {
+	uint32_t  nic_id;
+
+	// < max number of servers = 8.
+	uint32_t  number_of_servers;
+	
+	// Physical addresses, all!
+	//    need to be double-word aligned.
+	uint64_t  free_queue_address;
+	uint64_t  free_queue_lock_address; 
+	uint64_t  free_queue_buffer_address; 
+
+	uint64_t  rx_queue_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+	uint64_t  rx_queue_lock_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+	uint64_t  rx_queue_buffer_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+
+	uint64_t  tx_queue_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+	uint64_t  tx_queue_lock_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+	uint64_t  tx_queue_buffer_addresses[NIC_MAX_NUMBER_OF_SERVERS];
+
+} NicConfiguration;
+
+
+uint32_t getGlobalNicRegisterBasePointer ();
+
+// return 0 on success.
+int setGlobalNicRegisterBasePointer(uint32_t ptr);
+
+
+
 
 //
 // translate the queue related virtual addresses to physical, 
@@ -86,6 +134,7 @@ uint64_t getQueueBufferPhysicalAddressFromNic (NicCortosQueue* qptr);
 //
 void writeToNicReg (uint32_t nic_id, uint32_t reg_index, uint32_t reg_value);
 uint32_t readFromNicReg (uint32_t nic_id, uint32_t reg_index);
+uint32_t accessNicReg (uint8_t rwbar, uint32_t nic_id, uint32_t reg_index, uint32_t reg_value);
 
 void     setPhysicalAddressInNicRegPair (uint32_t nic_id, uint32_t reg_index, uint64_t pa);
 uint64_t getPhysicalAddressInNicRegPair (uint32_t nic_id, uint32_t reg_index);
@@ -102,5 +151,12 @@ void writeNicControlRegister   (uint32_t nic_id, uint32_t enable_flags);
 void setNicQueuePhysicalAddresses (uint32_t nic_id, uint32_t server_id,
 						uint32_t queue_type, uint64_t queue_addr, 
 						uint64_t queue_lock_addr, uint64_t queue_buffer_addr);
+void getNicQueuePhysicalAddresses (uint32_t nic_id, uint32_t server_id,
+		uint32_t queue_type,  uint64_t *queue_addr, 
+		uint64_t *queue_lock_addr, uint64_t *queue_buffer_addr);
+
+void configureNic (NicConfiguration* config);
+void enableNic  (uint32_t nic_id, uint8_t enable_interrupt, uint8_t enable_nic);
+void disableNic (uint32_t nic_id);
 		
 #endif
