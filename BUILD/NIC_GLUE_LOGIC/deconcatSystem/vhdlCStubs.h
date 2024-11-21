e0c8ad9efc7e678fd044f2c55893a63c3cdb103c
Bhupendra <22m1170@iitb.ac.in>
2024-11-21 19:43:07 +0530

    updated nic1.1 main code

--- BUILD/NIC_GLUE_LOGIC/deconcatSystem/vhdlCStubs.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <Pipes.h>
-#include <SocketLib.h>
-void global_storage_initializer_();
-void tx_deconcat();
